2b6901e1a3660b1e30f90358e08233811ca178cc
Durlabhkashyap <145114675+Durlabhkashyap@users.noreply.github.com>
2023-10-02 22:21:16 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 # Fillout The variables in Config.py further queries @riz4d 0n telegram
 
-BOT_TOKEN = "3db512869e08ce4dc4765a18c11470a2"
+BOT_TOKEN = "6386477338:AAFTs9_Nwoq4EMQx1n48n9V5Qrg7NYBDtEw"
 API_ID = 28586296
-API_HASH = "6386477338:AAFTs9_Nwoq4EMQx1n48n9V5Qrg7NYBDtEw"
+API_HASH = "3db512869e08ce4dc4765a18c11470a2"
 
 # If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
 
